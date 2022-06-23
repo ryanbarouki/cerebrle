206d7530c29eea5837d8a490f6a204ac9217eee7
aiidanm <iiiaiid4niii@gmail.com>
2022-06-23 20:48:44 +0100

    Started work on number guess page
    Added generate random number feature
    click to change Number
    user input field

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import seedrandom from 'seedrandom';
 import styled from 'styled-components';
 import { DateTime } from 'luxon';
 import cerebrleLogo from './cerebrle_logo.svg';
+import React from 'react';
+import { useState } from 'react';
 
 
 const BigContainer = styled.div`
@@ -19,24 +21,50 @@ const BigContainer = styled.div`
   }
 `;
 
-const Logo = styled.img`
-  height: 5rem;
-  pointer-events: none;
-  font-family: "Boston-Regular";
+
+const TargetNumber = styled.h1`
+  color: lightblue;
+  font-size: 3rem;
 `;
+
+const GenerateButton = styled.button`
+  color: lightblue;
+  background-color: whitesmoke;
+  margin: 1rem;
+`;
+
+const Input = styled.input`
+  background-color: whitesmoke;
+  text-align: center;
+`;
+
+const GenerateNumber = () => {
+  const [num, setNum] = useState(0);
+
+  function RandomNumberInRange(min, max) {
+    return Math.floor(Math.random() * (max - min +1)) + min;
+  };
+
+  const handleClick = () => {
+    setNum(RandomNumberInRange(1, 999999999));
+  };
+
+  return (
+    <BigContainer>
+      <TargetNumber>{num}</TargetNumber>
+      <Input type="text" placeholder="enter your guess"></Input>
+      <GenerateButton onClick={handleClick}>Change Number</GenerateButton>
+    </BigContainer>
+
+  );
+
+};
 
 
 const getDayString = () => {
   return DateTime.now().toFormat("yyyy-MM-dd");
 };
 
-function App() {
 
-  return (
-    <BigContainer>
-      <Logo src={cerebrleLogo} alt="logo" />            
-    </BigContainer>
-  );
-}
 
-export default App;
+export default GenerateNumber;
