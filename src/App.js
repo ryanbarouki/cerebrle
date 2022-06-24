9a5fa511cf2f472696c8897b8e24ff7b0fafc8c1
Ryan <ryanbarouki@gmail.com>
2022-06-24 10:55:59 +0100

    split out number game stuff

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import seedrandom from 'seedrandom';
 import styled from 'styled-components';
 import { DateTime } from 'luxon';
 import cerebrleLogo from './cerebrle_logo.svg';
-import React from 'react';
-import { useState } from 'react';
 
 
 const BigContainer = styled.div`
@@ -21,50 +19,24 @@ const BigContainer = styled.div`
   }
 `;
 
-
-const TargetNumber = styled.h1`
-  color: lightblue;
-  font-size: 3rem;
+const Logo = styled.img`
+  height: 5rem;
+  pointer-events: none;
+  font-family: "Boston-Regular";
 `;
-
-const GenerateButton = styled.button`
-  color: lightblue;
-  background-color: whitesmoke;
-  margin: 1rem;
-`;
-
-const Input = styled.input`
-  background-color: whitesmoke;
-  text-align: center;
-`;
-
-const GenerateNumber = () => {
-  const [num, setNum] = useState(0);
-
-  function RandomNumberInRange(min, max) {
-    return Math.floor(Math.random() * (max - min +1)) + min;
-  };
-
-  const handleClick = () => {
-    setNum(RandomNumberInRange(1, 999999999));
-  };
-
-  return (
-    <BigContainer>
-      <TargetNumber>{num}</TargetNumber>
-      <Input type="text" placeholder="enter your guess"></Input>
-      <GenerateButton onClick={handleClick}>Change Number</GenerateButton>
-    </BigContainer>
-
-  );
-
-};
 
 
 const getDayString = () => {
   return DateTime.now().toFormat("yyyy-MM-dd");
 };
 
+function App() {
 
+  return (
+    <BigContainer>
+      <Logo src={cerebrleLogo} alt="logo" />            
+    </BigContainer>
+  );
+}
 
-export default GenerateNumber;
+export default App;
