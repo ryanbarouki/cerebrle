32fe647e3580a62fddba39f57be52b965d0bf5db
Ryan <ryanbarouki@gmail.com>
2022-07-10 15:10:38 +0100

    start of info page

---
+++ src/components/Info.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
+export const Info = () => {
+  return (
+    <Container>
+      <div>How to play sequence memory etc...</div>
+    </Container>
+  );
+}
